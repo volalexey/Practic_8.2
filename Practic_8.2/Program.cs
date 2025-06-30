7f8d0b43c00d69625cbea8e3e3d54a466ed4b6aa
volalexey <sempayfox@gmail.com>
2025-06-30 17:22:09 +0300

    Add project files.

---
+++ Practic_8.2/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Practic_8._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
